a63e14407179d3bc9860b366b04b05f5e06921fc
Agustin-Balcazar <agus.b.reyes@gmail.com>
2024-04-26 04:05:01 -0600

    initial commit

--- js/listaUsuarios.js
+++ js/listaUsuarios.js
@@ -1,22 +1,3 @@
-/*var arreglo=[];
-arreglo[0]=3;
-arreglo[5]="Hola";
-arreglo.push("Mundo");
-var usuario={nombre:"Uriel Perez",
-            correo:"uriel@gmail.com",
-            telefono:'1234567890'};
-usuario.password="123";
-var usuario2={};
-usuario2.nombre="Karina";
-arreglo.push(usuario);
-arreglo.push(usuario2);
-console.log(arreglo);
-*/
-//JSON (JavaScript Object Notation)
-//{atributo1:valor,atributo2:valor}
-/*sessionStorage (tiempo de vida limitado al cierre del navegador)
-localStorage (tiempo de vida dependiente de que el usuario borre)*/
-
 document.addEventListener("DOMContentLoaded", () => {
     inicializarDatos();
     cargarTabla();
@@ -45,7 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     document.getElementById("btnAceptar").addEventListener("click", e => {
         //document.getElementById("msgDuplicado").classList.remove("show");
-        debugger;
+        //debugger;
         let alert = e.target.parentElement.querySelector(".alert");
         if (alert) {
             alert.remove();
@@ -197,14 +178,12 @@ function cargarTabla() {
         fila.appendChild(celda);
 
         celda = document.createElement("td");
-        celda.innerHTML = '<button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#mdlRegistro" value="'+usuario.correo+'" onclick="editar(' + i + ')">Editar</button>';
+        celda.innerHTML = '<button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#mdlUpdate" value="' + usuario.correo + '" onclick="editar(' + i + ')">Editar</button>'
+            + '<button type = "button" class="btn btn-warning"  data-bs-toggle="modal" data-bs-target="#mdlDelete" value = "' + usuario.correo + '" onclick = "editar(' + i + ')">Eliminar</button>'
+            + '<button type="button" class="btn btn btn-success" data-bs-toggle="modal" data-bs-target="#mdlResetPassword" value="' + usuario.correo + '" onclick="editar(' + i + ')">Restablecer contraseña</button>';
         fila.appendChild(celda);
         tbody.appendChild(fila);
     }
-
-}
-
-function editar(correo) {
 
 }
 
