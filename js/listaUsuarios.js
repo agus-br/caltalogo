8e6cd8b7c61d6ecd209ef74b1a2746115adca305
Agustin-Balcazar <agus.b.reyes@gmail.com>
2024-04-28 12:40:30 -0600

    nuevas funcionalidades

--- js/listaUsuarios.js
+++ js/listaUsuarios.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => { //Se ejecuta cuando el DOM ya esta cargado
     inicializarDatos();
     cargarTabla();
+    let usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    console.log(usuarios);    
+
     document.getElementById("txtNombre").onkeyup = e => revisarControl(e.target, 2, 60, "Campo no valido"); //Funcion anonima que manda llamar otra función
     document.getElementById("txtTelefono").onkeyup = e => { //onkeyup = evento que suelta la tecla que yo este presionando
         if (e.target.value.trim().length > 0) //trim = Borra espacios en blanco al inicio y al final.
@@ -24,6 +27,7 @@ document.addEventListener("DOMContentLoaded", () => { //Se ejecuta cuando el DOM
         });
         //console.log(controles);
     });
+
     document.getElementById("btnAceptar").addEventListener("click", e => {
         //document.getElementById("msgDuplicado").classList.remove("show");
         //debugger;
@@ -39,44 +43,18 @@ document.addEventListener("DOMContentLoaded", () => { //Se ejecuta cuando el DOM
         let txtContrasenia2 = document.getElementById("txtConfirmarPassword");
         let txtEmail = document.getElementById("txtEmail");
         let txtTel = document.getElementById("txtTelefono");
-        //Quitar las validaciones
-        /*
-        txtNombre.setCustomValidity("");
-        txtContrasenia.setCustomValidity("");
-        txtContrasenia2.setCustomValidity("");
-        txtEmail.setCustomValidity("");
-        txtTel.setCustomValidity("");
-        */
-       /*
-        if (txtNombre.value.trim().length < 2 ||
-            txtNombre.value.trim().length > 60) {
-            txtNombre.setCustomValidity("El nombre es obligatorio (entre 2 y 60 caracteres)");
-        }
-        if (txtContrasenia.value.trim().length < 6 ||
-            txtContrasenia.value.trim().length > 20) {
-            txtContrasenia.setCustomValidity("La contraseña es obligatoria (entre 2 y 60 caracteres)");
-        }
-        if (txtContrasenia2.value.trim().length < 6 ||
-            txtContrasenia2.value.trim().length > 20) {
-            txtContrasenia2.setCustomValidity("Confirma la contraseña");
-        }
-
-        if (txtTel.value.trim().length > 0 && txtTel.value.trim().length != 10) {
-            txtTel.setCustomValidity("El teléfono debe tener 10 dígitos");
-        }
-        */
-
+        
         revisarControl(txtNombre, 2, 60, "El nombre es obligatorio (entre 2 y 60 caracteres)");
         revisarControl(txtContrasenia, 2, 60, "La contraseña es obligatoria (entre 2 y 60 caracteres)");
 
-        //validarCorreo(txtEmail);
+        validarCorreo(txtEmail);
         validarContrasenias(txtContrasenia, txtContrasenia2);
         validarTelefono(txtTel);
-        txtEmail.setCustomValidity("");
-
+        //txtEmail.setCustomValidity("");
         if (e.target.form.checkValidity()) {
+            console.log("validación completa Aceptar")
             //Crear el objeto usuario y guardarlo en el storage
-            let correo=document.getElementById("txtCorreoOriginal").value.trim();
+            //let correo=document.querySelector("#mdlRegistro #txtCorreoOriginal").value.trim();
             let usuario = {
                 nombre: txtNombre.value.trim(),
                 correo: txtEmail.value.trim(),
@@ -85,106 +63,148 @@ document.addEventListener("DOMContentLoaded", () => { //Se ejecuta cuando el DOM
             };
             
             let usuarios = JSON.parse(localStorage.getItem("usuarios"));
-            if(document.querySelector("#mdlRegistro .modal-title").innerText=='Agregar')
+            let usuarioEncontrado = usuarios.find((element) => usuario.correo == element.correo);
+            if (usuarioEncontrado) 
             {
-                let usuarioEncontrado = usuarios.find((element) => usuario.correo == element.correo);
-                if (usuarioEncontrado) 
-                {
-                    
-                    let alerta = document.createElement('div');
-                    alerta.innerHTML = 'Este correo ya se encuentra registrado, favor de usar otro <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-                    alerta.className = "alert alert-warning alert-dismissible fade show";
-                    
+                let alerta = document.createElement('div');
+                alerta.innerHTML = 'Este correo ya se encuentra registrado, favor de usar otro <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+                alerta.className = "alert alert-warning alert-dismissible fade show";
+                
+                e.target.parentElement.insertBefore(alerta, e.target);
+                //e.target.parentElement.innerHTML=alerta+e.target.parentElement.innerHTML;
+                //debugger;
+                //document.getElementById("msgDuplicado").classList.add("show");
+                setTimeout(() => {
+                    //Destruir la alerta
+                    alerta.remove();
+                }, 3000);
 
-                    e.target.parentElement.insertBefore(alerta, e.target);
-                    //e.target.parentElement.innerHTML=alerta+e.target.parentElement.innerHTML;
-                    //debugger;
-                    //document.getElementById("msgDuplicado").classList.add("show");
-                    setTimeout(() => {
-                        //Destruir la alerta
-                        alerta.remove();
-                    }, 3000);
-
-                    e.preventDefault(); //Sirve para que no se recargue 
-                    return;
-                }
-                usuarios.push(usuario);
-                localStorage.setItem('usuarios', JSON.stringify(usuarios));
-            }else{
-                //Verifica que el correo actual sea diferente del correo original
-                if(usuario.correo!=correo){
-                    let usuarioEncontrado = usuarios.find((element) => usuario.correo == element.correo);
-                    if (usuarioEncontrado) {
-                        let alerta = document.createElement('div');
-                        alerta.innerHTML = 'Este correo ya se encuentra registrado, favor de usar otro <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
-                        alerta.className = "alert alert-warning alert-dismissible fade show";
-                        
-                        e.target.parentElement.insertBefore(alerta, e.target);
-                    
-                        setTimeout(() => {
-                            alerta.remove();
-                        }, 3000);
-    
-                        e.preventDefault();
-                        return;
-                    }
-                }
-
-                localStorage.setItem(correo, JSON.stringify(usuarios)); 
+                e.preventDefault(); //Sirve para que no se recargue 
+                return;
             }
-            
-
-            /*const mdlRegistro = bootstrap.Modal.getInstance('#mdlRegistro');
-            mdlRegistro.hide();*/
+            agregarUsuario(usuario);
         } else {
+            console.log("Error de validación");
             e.preventDefault();
-        }
-        
+        }  
     });
 
     document.getElementById("mdlRegistro").addEventListener('shown.bs.modal', (e) => {
         document.getElementById("btnLimpiar").click();
-        let operacion=e.relatedTarget.innerText;
-        
-        e.target.querySelector(".modal-title").innerText=operacion;
-
-        //Identificar si vamos editar para cargar los datos
-        if(operacion=='Editar'){
-            let correo=e.relatedTarget.value;
-            let usuarios=JSON.parse(localStorage.getItem('usuarios'));
-            let usuario=usuarios.find((element=>element.correo==correo));
-            document.getElementById("txtNombre").value=usuario.nombre;
-            document.getElementById("txtEmail").value=usuario.correo;
-            document.getElementById("txtCorreoOriginal").value=usuario.correo;
-            document.getElementById("txtTelefono").value=usuario.telefono;
-            localStorage.setItem("usuarios", JSON.stringify(usuario));
-        }
+        e.target.querySelector("#mdlRegistro .modal-title").innerText="Agregar";
         document.getElementById("txtNombre").focus();
-        
     })
-});
 
-function editar(id) {
     document.getElementById("mdlUpdate").addEventListener('shown.bs.modal', (e) => {
-        //document.getElementById("btnLimpiar").click();
-        //let operacion=e.relatedTarget.innerText;
-        
-        e.target.querySelector(".modal-title").innerText=Eliminar;
+        document.getElementById("btnLimpiar").click();
+        e.target.querySelector("#mdlUpdate .modal-title").innerText="Editar";
 
         //Identificar si vamos editar para cargar los datos
-        if(operacion=='Editar'){
-            let correo=e.relatedTarget.value;
-            let usuarios=JSON.parse(localStorage.getItem('usuarios'));
-            let usuario=usuarios.find((element=>element.correo==correo));
-            document.getElementById("txtNombre").value=usuario.nombre;
-            document.getElementById("txtEmail").value=usuario.correo;
-            document.getElementById("txtCorreoOriginal").value=usuario.correo;
-            document.getElementById("txtTelefono").value=usuario.telefono;
-        }
-        document.getElementById("txtNombre").focus();
+        let correo = e.relatedTarget.value;
+        let usuarios=JSON.parse(localStorage.getItem('usuarios'));
+        let usuario = usuarios.find((element => element.correo == correo));
+        
+        //console.log(usuario)
+        document.getElementById("txtNombreUpdate").value=usuario.nombre;
+        document.getElementById("txtEmailUpdate").value=usuario.correo;
+        document.getElementById("txtCorreoOriginal").value = usuario.correo;
+        document.getElementById("txtTelefonoUpdate").value = usuario.telefono;
+        
+        document.getElementById("txtNombreUpdate").focus();
         
     })
-}
+
+    document.getElementById("btnActualizar").addEventListener("click", e => {
+        //document.getElementById("msgDuplicado").classList.remove("show");
+        //debugger;
+        let alert = e.target.parentElement.querySelector("#mdlUpdate .alert");
+        if (alert) {
+            alert.remove();
+        }
+        //Agregar la clase validado al formulario padre del boton que desencadeno el click
+        e.target.form.classList.add("validado");
+
+        //Obteniendo los elementos
+        let txtNombre = document.getElementById("txtNombreUpdate");
+        //let txtContrasenia = document.getElementById("txtPassword");
+        //let txtContrasenia2 = document.getElementById("txtConfirmarPassword");
+        let txtEmail = document.getElementById("txtEmailUpdate");
+        let txtTel = document.getElementById("txtTelefonoUpdate");
+        
+        revisarControl(txtNombre, 2, 60, "El nombre es obligatorio (entre 2 y 60 caracteres)");
+        //revisarControl(txtContrasenia, 2, 60, "La contraseña es obligatoria (entre 2 y 60 caracteres)");
+        validarCorreo(txtEmail);
+        //validarContrasenias(txtContrasenia, txtContrasenia2);
+        validarTelefono(txtTel);
+        //txtEmail.setCustomValidity("");
+        
+        if (e.target.form.checkValidity()) {
+            //Crear el objeto usuario y guardarlo en el storage
+            let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+            let correoAnterior = document.getElementById("txtCorreoOriginal").value.trim();
+            let correoNuevo = txtEmail.value.trim()
+                     
+            if (correoNuevo != correoAnterior) {
+                let usuarioEncontrado = usuarios.find((element) => correoNuevo == element.correo);
+                if (usuarioEncontrado) {
+                    let alerta = document.createElement('div');
+                    alerta.innerHTML = 'Este correo ya se encuentra registrado, favor de usar otro <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>';
+                    alerta.className = "alert alert-warning alert-dismissible fade show";
+                    
+                    e.target.parentElement.insertBefore(alerta, e.target);
+                    
+                    setTimeout(() => {
+                        alerta.remove();
+                    }, 3000);
+                    
+                    e.preventDefault();
+                    return;
+                }
+            }
+            let indiceUsuario = usuarios.findIndex(usuario => usuario.correo === correoAnterior);
+            let usuario = {
+                nombre: txtNombre.value.trim(),
+                correo: correoNuevo,
+                contrasenia: usuarios[indiceUsuario].contrasenia,
+                telefono: txtTel.value.trim()
+            }; 
+            usuarios[indiceUsuario] = usuario;
+            // Guarda el array actualizado en el localStorage
+            localStorage.setItem('usuarios', JSON.stringify(usuarios));
+            //console.log("FINAL LOCALSTORAGE")
+            //console
+        } else {
+            console.log("Error de validación");
+            e.preventDefault();
+        }  
+    });
+
+    document.getElementById("mdlDelete").addEventListener('shown.bs.modal', (e) => {
+
+        e.target.querySelector("#mdlDelete .modal-title").innerText = "Eliminar";
+
+        //Identificar si vamos editar para cargar los datos
+        let correo = e.relatedTarget.value;
+        let usuarios = JSON.parse(localStorage.getItem('usuarios'));
+        let usuario = usuarios.find((element => element.correo == correo));
+        //console.log(usuario)
+        document.getElementById("showUser").innerText = "Esta seguro de eliminar al usuario: " + usuario.nombre;
+        /*document.getElementById("showUser").innerHTML  = "<div>Esta seguro de eliminar al usuario: " + usuario.nombre + "</div>";*/
+    });
+
+    document.getElementById("btnEliminar").addEventListener("click", e => {
+        //Agregar la clase validado al formulario padre del boton que desencadeno el click
+        e.target.form.classList.add("validado");
+
+        let delteUserEmail = e.target.value.trim();
+        
+        console.log(e.target.value)
+        e.preventDefault();
+        return;
+        localStorage.removeItem(delteUserEmail);
+    });
+
+});
 
 function revisarControl(control, min, max, message) {
     //Limpiar mensajes de validacion
@@ -205,23 +225,14 @@ function revisarControl(control, min, max, message) {
     console.log(txt.validity);*/
 }
 
-function eliminar(){
-
-    //Traemos los datos del localStorage
-    let usuarios = JSON.parse(localStorage.getItem('usuarios'));
-
-
-
-}
-
 function validarCorreo(control) {
     const regexCorreo = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
+    
     control.setCustomValidity(""); //setCustomValidity = No muestre un mensaje de validacion
     control.classList.remove("valido"); //
     control.classList.remove("novalido");
     //Manera visual de ver que el usuario metio los datos incorrectos
-    if (control.value.trim().match(regexCorreo)) {
+    if (!control.value.trim().match(regexCorreo)) {
         control.setCustomValidity("El correo que ingreso no cumple con el formato");
         //txt.setCustomValidity("Campo no válido");
         control.classList.add("novalido");
@@ -282,27 +293,26 @@ function cargarTabla() {
         fila.appendChild(celda);
 
         celda = document.createElement("td");
-        celda.innerHTML = '<button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#mdlRegistro" value="' + usuario.correo + '" onclick="editar(' + i + ')">Editar</button>'
+        celda.innerHTML = '<button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#mdlUpdate" value="' + usuario.correo + '">Editar</button>'
         fila.appendChild(celda);
-        
 
         celda = document.createElement("td");
-        celda.innerHTML = '<button type = "button" class="btn btn-danger"  data-bs-toggle="modal" data-bs-target="#mdlDelete" value = "' + usuario.correo + '" onclick = "editar(' + i + ')">Eliminar</button>';
+        celda.innerHTML ='<button type="button" class="btn btn-primary"  data-bs-toggle="modal" data-bs-target="#mdlDelete" value="' + usuario.correo + '">Eliminar</button>';
         fila.appendChild(celda);
-        
 
         celda = document.createElement("td");
-        celda.innerHTML = 
-        '<button type="button" class="btn btn btn-warning" data-bs-toggle="modal" data-bs-target="#mdlResetPassword" value="' + usuario.correo + '" onclick="resetPassword(' + i + ')">Restablecer contraseña</button>';
+        celda.innerHTML ='<button type="button" class="btn btn btn-warning" data-bs-toggle="modal" data-bs-target="#mdlResetPassword" value="' + usuario.correo + '">Restablecer contraseña</button>';
         fila.appendChild(celda);
+
         tbody.appendChild(fila);
     }
 
 }
 
 function inicializarDatos() {
-    let usuarios = localStorage.getItem('usuarios'); //localstorage = base de datos local mientras la sesión del navegador esta abierto
-    if (!usuarios) {
+    let usuarios = localStorage.getItem('usuarios');
+    if (!usuarios || !Array.isArray(JSON.parse(usuarios))) {
+        // Si no hay usuarios en el localStorage o no es un array válido, inicializa el array de usuarios con los predeterminados
         usuarios = [
             {
                 nombre: 'Uriel Perez Gomez',
@@ -318,24 +328,13 @@ function inicializarDatos() {
             }
         ];
 
-        //let usuarios=[];
-        //push sirve para meter elementos a la lista
-        usuarios.push(
-            {
-                nombre: 'Uriel Perez Gomez',
-                correo: 'uriel1@gmail.com',
-                password: '123456',
-                telefono: ''
-            });
-        usuarios.push(
-            {
-                nombre: 'Lorena Garcia Hernandez',
-                correo: 'lorena1@gmail.com',
-                password: '567890',
-                telefono: '4454577468'
-            });
-
-        localStorage.setItem('usuarios', JSON.stringify(usuarios)); //JSON.stringify lo guarda en formato JSON
-
+        // Guardar los usuarios en el almacenamiento local
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
     }
+}
+
+function agregarUsuario(usuario) {
+    let usuarios = JSON.parse(localStorage.getItem('usuarios'));
+    usuarios.push(usuario);
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
 }
